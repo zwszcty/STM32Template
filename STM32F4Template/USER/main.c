540fbea104f7306d270246e62ab265b684dd9bc2
zwszcty <zwszcty@outlook.com>
2023-11-01 13:23:37 +0800

    first commit

---
+++ STM32F4Template/USER/main.c
@@ -0,0 +1,27 @@
+#include "stm32f4xx.h"
+#include "usart.h"
+#include "delay.h"
+
+ 
+//STM32F4工程-库函数版本
+//淘宝店铺：http://mcudev.taobao.com
+
+
+int main(void)
+{
+	u32 t=0;
+	uart_init(115200);
+	delay_init(84);
+	
+  while(1){
+    printf("t:%d\r\n",t);
+		delay_ms(500);
+		t++;
+	}
+}
+
+/*
+//STM32F4工程-库函数版本
+//淘宝店铺：http://mcudev.taobao.com
+*/
+
